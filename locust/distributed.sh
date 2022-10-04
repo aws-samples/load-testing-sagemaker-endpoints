0b6eaa0b06de1d33f77d227010ea1a65fe7e9606
Marc <marckarp101@gmail.com>
2022-10-04 00:35:54 +0000

    Add locust_file and shell script

---
+++ locust/distributed.sh
@@ -0,0 +1,42 @@
+#replace with your endpoint name
+export ENDPOINT_NAME=https://huggingface-pytorch-inference-2022-07-25-17-21-31-881
+
+export REGION=us-east-1
+export CONTENT_TYPE=application/json
+export PAYLOAD={"inputs": "I am super happy right now."}
+
+#replace with the locust script that you are testing, this is the locust_script that will be used to make the InvokeEndpoint API calls. 
+export SCRIPT=locust_script.py
+#make sure you are in a virtual environment
+#. ./venv/bin/activate
+locust -f $SCRIPT -H $ENDPOINT_NAME --master --expect-workers=30 -u 10 -t 1m --csv results --headless &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
+locust -f $SCRIPT -H $ENDPOINT_NAME --worker --master-host=localhost &
